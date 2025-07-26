5caf5074a699ae20f4bbbfb1d8afc56592fd5b1c
pagaldani890 <pagal.dani.89@gmail.com>
2025-07-27 01:25:53 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~I6EhBSTY#Vwsm64_72oGKveDDuEQhGXC-kCrIwMQgYn-T67ADTkE",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
